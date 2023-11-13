c9ce2d65668b13a3a2fa26649cbae8bd2b976644
Andrew Lau <63950447+Andrew0000000@users.noreply.github.com>
2023-11-13 21:34:06 +0000

    created module history graph

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from dash import Dash, html, Output, Input
 from demoGraph import demoGraph, demoGraphLayout
-from requestedVsRecruitedGraph import requestedVsRecruitedGraph, requestedVsRecruitedGraphLayout
+from requestedVsRecruitedGraph import requestedVsRecruitedGraph, requestedVsRecruitedGraphLayout, moduleHistoryGraphLayout, moduleHistoryGraph
 
 app = Dash(__name__)
 server = app.server
@@ -9,8 +9,12 @@ server = app.server
 app.layout = html.Div([
     demoGraphLayout(),
     html.Hr(),
-    requestedVsRecruitedGraphLayout()
+    requestedVsRecruitedGraphLayout(),
+    html.Hr(),
+    moduleHistoryGraphLayout()
 ])
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Callback for demoGraph
 @app.callback(
@@ -18,11 +22,12 @@ app.layout = html.Div([
     Input(component_id='demoGraphRadioItem', component_property='value')
 )
 
-# Callback for requestedVsRecruitedGraph
-
 def update_demoGraph(col_chosen):
     return demoGraph(col_chosen)
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Callback for requestedVsRecruitedGraph
 @app.callback(
     Output(component_id='requestedVsRecruitedGraph', component_property='figure'),
     Input(component_id='requestedVsRecruitedGraphDropdown' , component_property='value')
@@ -30,6 +35,17 @@ def update_demoGraph(col_chosen):
 
 def update_requestedVsRecruitedGraph(selected_year):
     return requestedVsRecruitedGraph(selected_year)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Callback for moduleHistoryGraph
+@app.callback(
+    Output(component_id='moduleHistoryGraph', component_property='figure'),
+    Input(component_id='moduleHistoryGraphDropdown' , component_property='value')
+)
+
+def update_moduleHistoryGraph(selected_module):
+    return moduleHistoryGraph(selected_module)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/requestedVsRecruitedGraph.py
+++ src/requestedVsRecruitedGraph.py
@@ -87,7 +87,73 @@ def requestedVsRecruitedGraph(selected_year):
         xaxis_title='Count',
         yaxis_title='Module Code',
         hovermode='closest',
-        height=5000,  # Adjust the height as needed
-        width=1800  # Adjust the width as needed
+        height=5000,
+        width=1800
     )
     return go.Figure(data=[trace1, trace2], layout=layout)
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  Graph with dropdown of modules, showing selected module's history of recruited vs requested
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+def moduleHistoryGraphLayout():
+    # Get a list of unique modules from the dataframe
+    modules = df['Module Code'].unique().tolist()
+    options = [{'label': module, 'value': module} for module in modules]
+    
+    return html.Div([
+        dcc.Dropdown(
+            options=options,
+            value=modules[0],  # Default value set to the first module in the list
+            id='moduleHistoryGraphDropdown'
+        ),
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': col, 'id': col} for col in df.columns if col not in ['Module Code and Title']],
+            page_size=10
+        ),
+        dcc.Graph(figure={}, id='moduleHistoryGraph')
+    ])
+
+def moduleHistoryGraph(selected_module):
+    # Filter the dataframe for the selected module
+    module_data = df[df['Module Code'] == selected_module]
+
+    traces = []
+    for year in ['2021-22', '2022-23', '2023-24']:
+        # Calculate the difference for each year
+        module_data['Difference'] = module_data[year + ' requested'] - module_data[year + ' recruited']
+        colors = module_data['Difference'].apply(lambda x: 'red' if x < 0 else 'green')
+
+        # Create traces for each year
+        trace_recruited = go.Bar(
+            x=[module_data[year + ' recruited']],
+            y=[year],
+            name=f'Recruited {year}',
+            marker_color=colors.values[0],
+            orientation='h'
+        )
+        trace_requested = go.Bar(
+            x=[module_data[year + ' requested']],
+            y=[year],
+            name=f'Requested {year}',
+            marker_color=colors.values[0],
+            orientation='h'
+        )
+        traces.extend([trace_recruited, trace_requested])
+
+    layout = go.Layout(
+        title=f'Comparison of Requested vs. Recruited Students for {selected_module}',
+        barmode='group',
+        xaxis_title='Count',
+        yaxis_title='Year',
+        hovermode='closest',
+        height=600,
+        width=800
+    )
+    return go.Figure(data=traces, layout=layout)
